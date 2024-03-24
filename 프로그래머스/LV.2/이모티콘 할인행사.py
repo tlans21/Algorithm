c7ca9dcf4bdee890faa1281956daf51de79dd1dd
tlans21 <cjstlans21@naver.com>
2024-03-24 19:43:45 +0900

    2024-03-24

--- 프로그래머스/LV.2/이모티콘 할인행사.py
+++ 프로그래머스/LV.2/이모티콘 할인행사.py
@@ -1,6 +1,7 @@
 from itertools import product
+
 def solution(users, emoticons):
-    answer = []
+    answer = [0, 0]
     # 가입자수를 높이기 위해서는 할인율을 최소한으로 해야한다.
     # 할인율로 인한 손실을 최대한 막기 위해 이모티콘 가격이 낮은순으로 정렬
     emoticon = sorted(emoticons)
@@ -8,35 +9,24 @@ def solution(users, emoticons):
     ratio = [10, 20, 30, 40]
     prod_list = list(product(ratio, repeat=len(emoticons)))
     
-    plus = 0
     final_price = 0
+    max_sum = 0
     
-    for user in users:
-        user_ratio, user_price = user[0], user[1]
-        for prod in prod_list:
-            sum = 0
-            breakPoint = False
-            for idx, value in enumerate(prod):
-                # 할인율에 부합하지 않으므로 continue
-                if value < user_ratio:
-                    continue
-                # 계속 더하기    
-                if sum < user_price:
-                    sum += emoticons[idx] * ((100 - value) / 100)
-                else:
-                    # 가격을 넘었으니 플러스 구매
-                    breakPoint = True
-                    plus += 1
-                    break
+    for discounts in prod_list:
+        plus = 0
+        total_sum = 0
+        for user in users:
+            user_sum = 0
+            user_ratio, user_price = user[0], user[1]
+            for idx, value in enumerate(discounts):
+                if value >= user_ratio: 
+                    user_sum += emoticons[idx] * (100 - value) / 100
+                
+            if user_price > user_sum:
+                total_sum += user_sum
             else:
-                print(sum)
-                final_price += sum
-                
-                
-            if breakPoint == True:
-                break
+                plus += 1
+        
+        answer = max(answer, [plus, total_sum])
             
-    answer.append(plus)
-    answer.append(final_price)
-                
     return answer
