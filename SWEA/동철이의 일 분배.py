643bc9f1efd282b05f4cdd6926146d7734f0b8f4
tlans21 <cjstlans21@naver.com>
2024-05-29 16:08:39 +0900

    2024-05-29

--- SWEA/동철이의 일 분배.py
+++ SWEA/동철이의 일 분배.py
@@ -1,16 +1,39 @@
+def dfs(y, depth, per, visited):
+    global max_value
+    if depth == N-1:
+        value = 1
+        for i in range(len(per)):
+            value *= per[i] / 100
+       
+        value = value * 100
+        max_value = max(max_value, value)
+        return
 
+    
 
+    for ny in range(N):
+        if visited[ny] == 0:
+            visited[ny] = 1
+            per.append(board[depth + 1][ny])
+            dfs(ny, depth + 1, per, visited)
+            per.pop()
+            visited[ny] = 0
+        
 T = int(input())
 
 for tc in range(1, T+1):
+    max_value = -1
     N = int(input())
-    
     board = [list(map(int, input().split())) for _ in range(N)]
     
     # ex) board[0][2] 1번 사람이 3번 일을 성공확률
     for i in range(N):
-        for j in range(N):
-            percentage = board[i][j]
-
+        percentage = []
+        visit = [0 for _ in range(N)]
+        percentage.append(board[0][i])
+        visit[i] = 1
+        dfs(i, 0, percentage, visit)
+        visit[i] = 0
+    print("#{} {:.6f}".format(tc, round(max_value, 6)))
     
 
